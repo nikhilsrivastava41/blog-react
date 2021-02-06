628cf9ec85d10048be559ccdf0e8a95619fa13df
Nikhil Srivastava <nikhilsrivastava@Nikhils-MacBook-Air.local>
2021-02-06 21:53:36 +0530

    Blog Details ready

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,9 +1,19 @@
 import React from 'react'
-
+import { useParams } from 'react-router-dom'
+import useFetch from './useFetch.js';
 function BlogDetails() {
+    const {id} = useParams();
+    const {data, isPending}  = useFetch('http://localhost:8000/blogs' + id);
     return (
         <div className="blog-details">
-            <h2>Blog Details</h2>
+            {isPending && <div>Loading...</div>}
+            {data && (
+                <article>
+                    <h2>{data.title}</h2>
+                    <p>Written by {data.author}</p>
+                    <div>{data.body}</div>
+                </article>
+            )}
         </div>
     )
 }
